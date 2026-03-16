d1de8a9408f801e05646c354f22192a29e7c0553
Martin Laporte <mlaporte@coveo.com>
2026-03-16 11:46:45 +0100

    chore: bump version to 1.4.0

--- packages/treebeard/electrobun.config.ts
+++ packages/treebeard/electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
   app: {
     name: 'Treebeard',
     identifier: 'com.treebeard.app',
-    version: '1.3.1'
+    version: '1.4.0'
   },
   runtime: {
     exitOnLastWindowClosed: true
